c5bc0b9d68a4386b8f06f1bb0bf759eac7331cac
Drozd Aliaksei <aliaksei.drozd@yandex.ru>
2018-04-12 12:45:24 +0300

    clean code

--- app/src/main/java/cleverhouse/shms/MainActivity.java
+++ app/src/main/java/cleverhouse/shms/MainActivity.java
@@ -1,33 +1,27 @@
 package cleverhouse.shms;
 
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  implements CompoundButton.OnCheckedChangeListener , View.OnClickListener{
 
-    private TextView textView ;
+    //private TextView textView ;
     private Button btnConnect;
     private Switch switch3;
 
     String con1 , con2;
-
-    private final IntentFilter intentFilter = new IntentFilter();
-   // WifiP2pManager.Channel mChannel;
-   // WifiP2pManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        //textView = (TextView) findViewById(R.id.textView);
         btnConnect = (Button) findViewById(R.id.btnConnect);
         switch3 = (Switch) findViewById(R.id.switch3);
 
@@ -36,29 +30,12 @@ public class MainActivity extends AppCompatActivity  implements CompoundButton.O
         switch3.setText("Включить лампочку");
         switch3.setChecked(false);
         switch3.setOnCheckedChangeListener(this);
-
-/*
-        // Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-  */
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, Connect.class);
-        startActivityForResult(intent, 1);
+        startActivity(intent);
     }
 
     @Override
@@ -68,12 +45,12 @@ public class MainActivity extends AppCompatActivity  implements CompoundButton.O
         else
         {switch3.setText("Включить лампочку");}
     }
-
+/*
     protected void onActivityResult(int requestCode, int resultCode, Intent data ) {
         if (data == null) {return;}
         con1 = data.getStringExtra("connectionInfo1");
         con2 = data.getStringExtra("connectionInfo2");
-    }
+    }*/
 
 
 }
